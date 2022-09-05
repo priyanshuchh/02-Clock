3449c4a0e4d0ec2550c311976c098f00cb299821
Priyanshu Chhabra <pchhabra.chhabra@gmail.com>
2022-09-05 16:43:45 +0530

    init

---
+++ script.js
@@ -0,0 +1,20 @@
+const deg = 6;
+
+const hr = document.querySelector('#hr');
+const mn = document.querySelector('#mn');
+const sc = document.querySelector('#sc');
+
+setInterval(() => {
+    let day = new Date();
+
+    //setting the actual seconds minutes and hour in clock
+    let ms = day.getMilliseconds() * deg;
+    let ss = day.getSeconds() * deg + (ms / 1000);
+    let mm = day.getMinutes() * deg + ss / 60;
+    let hh = day.getHours() * 30;
+
+    //changing the degrees in the style as per the time
+    hr.style.transform = `rotateZ(${(hh) + (mm / 12)}deg)`;
+    mn.style.transform = `rotateZ(${mm}deg)`;
+    sc.style.transform = `rotateZ(${ss}deg)`;
+}, 1);
